b83e90191ff4af3218a1dd37e002686dce3e70a4
Ruhrozz <ruhrozz@bk.ru>
2024-11-08 23:56:31 +0700

    feat(hyprland): add neovide hotkey

--- home-manager/gui/hyprland/binds.nix
+++ home-manager/gui/hyprland/binds.nix
@@ -37,6 +37,7 @@
       "$mod, D, exec, nautilus"
       "$mod, T, exec, telegram-desktop"
       "$mod, O, exec, obsidian"
+      "$mod, N, exec, neovide"
       "$mod CTRL, S, exec, __NV_PRIME_RENDER_OFFLOAD=1 __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only steam"
       "$mod CTRL, O, exec, osu\!"
 
